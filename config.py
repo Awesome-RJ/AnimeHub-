ec48e02690a431f377dabc85088862e35be5a27e
Dankdel <79897294+Dankdel@users.noreply.github.com>
2021-03-26 08:49:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+{
+  "token": "TOKEN",
+  "db": {
+    "host": "localhost",
+    "port": 27017,
+    "db_name": "animehub"
+  },
+  "messages": {
+    "start": "Hi {}, Welcome to AnimeHub 🤖\n\n Anime-Scraper-Telegram-BotWatch or download anime without ads",
+    "menu": "Free Download Anime \n\n/download\n/recommend\n/latest\n/info\n/about\n/help",
+    "help": "Help for AnimeHub\n\n/start - start the bot\n/download - download or stream anime\n/recommend - get anime recommendations\n/latest - get latest anime releases\n/info - get info about an anime\n/about - about to the bot\n/help - learn how the bot works\n\nBot Users - {:,}\nAnime Downloaded - {:,}\nRecommendations Given - {:,}\nAnime Info Fetched - {:,}",
+    "recommend": "Enter the anime you want recommendations for ",
+    "empty_search": "We couldn't find the anime you searched for, check the spelling and try again 😣",
+    "empty_recommendation": "We couldn't find any recommendation for the anime you searched for, please search for another one 😣",
+    "unknown": "I don't understand your message ⁉️, please check /help for AnimeHive instructions",
+    "recommendation_search": "{} 😎\n\n{} - {} 📺\n{} ⏰",
+    "recommendation_result": "{} 😎\n\n{} 📺\n{} ⏰",
+    "download": "Enter the anime you want to download 😋",
+    "download_pagination": "AnimeHub scraped {} episodes for the requested anime (this includes HD, 360p, 480p, 720p and 1080p versions) 😁\n\nFor limiting purposes we only allow displaying of 10 episodes per request 😊",
+    "about": "About @Ast_red",
+    "get_info": "Enter the anime you want to get info for 😎",
+    "anime_info": "{}\n\n{}\n{}\n{}\n{}\n{}\n{}\nGenre: {}",
+    "select_episode": "Select the episode you want to download 😁",
+    "broadcast": "Enter the message you want to broadcast to all {} users 🤖"
+  }
+}
